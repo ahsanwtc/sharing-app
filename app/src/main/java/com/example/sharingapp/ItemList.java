400df920d70cdeedb094cba358b48c2985d49b64
jsan <ahsan.wtc@gmail.com>
2023-01-23 13:30:12 +0100

    adding observerable to itemlist

--- app/src/main/java/com/example/sharingapp/ItemList.java
+++ app/src/main/java/com/example/sharingapp/ItemList.java
@@ -17,7 +17,7 @@ import java.util.ArrayList;
 /**
  * ItemList class
  */
-public class ItemList {
+public class ItemList extends Observerable {
 
     private static ArrayList<Item> items;
     private String FILENAME = "items.sav";
@@ -28,6 +28,7 @@ public class ItemList {
 
     public void setItems(ArrayList<Item> item_list) {
         items = item_list;
+        notifyObservers();
     }
 
     public ArrayList<Item> getItems() {
@@ -36,10 +37,12 @@ public class ItemList {
 
     public void addItem(Item item) {
         items.add(item);
+        notifyObservers();
     }
 
     public void deleteItem(Item item) {
         items.remove(item);
+        notifyObservers();
     }
 
     public Item getItem(int index) {
@@ -62,7 +65,6 @@ public class ItemList {
     }
 
     public void loadItems(Context context) {
-
         try {
             FileInputStream fis = context.openFileInput(FILENAME);
             InputStreamReader isr = new InputStreamReader(fis);
@@ -75,6 +77,7 @@ public class ItemList {
         } catch (IOException e) {
             items = new ArrayList<Item>();
         }
+        notifyObservers();
     }
 
     public boolean saveItems(Context context) {
